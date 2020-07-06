126a43c62a8c1f1200babc823d4e5a0a9e147d62
Abner Machado <abner81@live.com>
2020-07-06 09:54:02 -0300

    Teste adiciona put api

--- router/saldosDecrescentesRouter.js
+++ router/saldosDecrescentesRouter.js
@@ -12,5 +12,15 @@ app.get("/:limitNumber", async (req, res) => {
     res.status(400).send("erro no saldos crescentes " + error);
   }
 });
+// teste git
+app.put("/:limitNumber", async (req, res) => {
+  try {
+    const num = parseInt(req.params.limitNumber, 10);
+    const account = await accountsModel.find().sort({ balance: -1, name: 1 }).limit(num);
+    res.send(account);
+  } catch (error) {
+    res.status(400).send("erro no saldos crescentes " + error);
+  }
+});
 
 export { app as saldosDecrescentesRouter };
